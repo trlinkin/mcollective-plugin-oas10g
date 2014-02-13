776f2e64052cbe29a8937d832b4d5f9b29bfd7e6
Thomas Linkin <trlinkin@gmail.com>
2014-02-13 15:35:30 -0500

    Add data manipulation for :app_status action

--- oas10g.rb
+++ oas10g.rb
@@ -8,6 +8,10 @@ module MCollective
         err = String.new
         status = run("/u03/devmidr2/opmn/bin/opmnctl status -noheaders -rsep ',' -fsep '|' -fmt %typ%prt%sta", :stdout => container_list, :stderr => err)
 
+        if not status == 0
+          fail!("Execution fail with error: #{err}")
+        end
+
         records = container_list.split(',')
         containers = Hash.new
 
@@ -45,6 +49,21 @@ module MCollective
         err = String.new
         status = run("/u03/devmidr2/dcm/bin/dcmctl listApplications #{options}", :stdout => apps_raw, :stderr => err)
 
+        if not status == 0
+          fail!("Execution fail with error: #{err}")
+        end
+
+
+        apps = apps_raw.split(/^\d/)
+        apps.reject!{&:empty?}
+
+        apps.map! do |app|
+          app.strip!
+          name = app.split(/\n/).first
+          name.strip
+        end
+
+        reply[:results]
       end
     end
   end
