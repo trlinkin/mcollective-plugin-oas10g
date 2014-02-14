0cb029662821b3a7f5d5d850c03fee916cae9485
Thomas Linkin <trlinkin@gmail.com>
2014-02-14 10:41:41 -0500

    Hand merge code from my pals

--- oas10g.ddl
+++ oas10g.ddl
@@ -49,3 +49,111 @@ action :app_status, :description => "Retrieves current status of an OAS applicat
          :description => "Application Status Results as hash, empty if no results are found",
          :display_as  => "Application Status Results"
 end
+
+action "deploy", :description => "Deploys specified application" do
+
+    input :ear_file,
+        :prompt         => "EAR File",
+        :description    => "EAR file to be deployed (include path)",
+        :type           => :string,
+        :validation     => '^[a-zA-Z\/.-_\d]+$',
+        :optional       => false,
+        :maxlength      => 30
+
+    input :c_name,
+        :prompt         =>  "Container Name",
+        :description    =>  "OAS Name for the container",
+        :type           =>  :string,
+        :optional       =>  false,
+        :validation     =>  '^[a-zA-Z\-_\d]+$',
+        :maxlength      =>  30
+
+    input :a_name,
+        :prompt         =>  "Application Name",
+        :description    =>  "OAS Name for the application",
+        :type           =>  :string,
+        :optional       =>  false,
+        :validation     =>  '^[a-zA-Z\-_\d]+$',
+        :maxlength      =>  30
+
+    output :pid,
+        :display_as     =>  "Process Identifier",
+        :description    =>  "Process Identifier of deploy process"
+
+    output :summary,
+        :display_as     =>  "Status/Summary",
+        :description    =>  "Status/Summary of Deploy task"
+end
+
+# This is Dan's UNDEPLOY action
+action "undeploy", :description => "Undeploys specified application" do
+    input :c_name,
+        :prompt         =>  "Container Name",
+        :description    =>  "OAS Name for the container",
+        :type           =>  :string,
+        :optional       =>  false,
+        :validation     =>  '^[a-zA-Z\-_\d]+$',
+        :maxlength      =>  30
+
+    input :a_name,
+        :prompt         =>  "Application Name",
+        :description    =>  "OAS Name for the application",
+        :type           =>  :string,
+        :optional       =>  false,
+        :validation     =>  '^[a-zA-Z\-_\d]+$',
+        :maxlength      =>  30
+
+    output :pid,
+        :display_as     =>  "Process Identifier",
+        :description    =>  "Process Identifier of undeploy process"
+
+    output :summary,
+        :display_as     =>  "Status/Summary",
+        :description    =>  "Status/Summary of Undeploy task"
+end
+
+# This is Dan's CONTAINER_START action
+action "container_start", :description => "Starts the specified container" do
+    input :c_name,
+        :prompt         =>  "Container Name",
+        :description    =>  "OAS Name for the container",
+        :type           =>  :string,
+        :optional       =>  false,
+        :validation     =>  '^[a-zA-Z\-_\d]+$',
+        :maxlength      =>  30
+
+    output :summary,
+        :display_as     =>  "Status/Summary",
+        :description    =>  "Status/Summary of Container Start task"
+end
+
+action "container_stop", :description => "Starts the specified container" do
+    input :c_name,
+        :prompt         =>  "Container Name",
+        :description    =>  "OAS Name for the container",
+        :type           =>  :string,
+        :optional       =>  false,
+        :validation     =>  '^[a-zA-Z\-_\d]+$',
+        :maxlength      =>  30
+
+    output :summary,
+        :display_as     =>  "Status/Summary",
+        :description    =>  "Status/Summary of Container Stop task"
+end
+
+# This is Dan's PIDCHECK action
+action "pidcheck", :description => "Checks to see if the specified PID exists (if a process is running)" do
+    input :pid,
+        :prompt         =>  "Process Identifier",
+        :description    =>  "Number that identifies a specific running process",
+        :type           =>  :integer,
+        :optional       =>  false
+
+    output :status,
+        :display_as     =>  "Status",
+        :description    =>  "Whether a process with specified PID is running"
+
+    output :status_b,
+        :display_as     =>  "Boolean Status",
+        :description    =>  "Status of PIDCHECK function in boolean form"
+end
--- oas10g.rb
+++ oas10g.rb
@@ -37,7 +37,7 @@ module MCollective
         reply[:results] = containers
       end
 
-###### Application Status Action
+      ###### Application Status Action
       action :app_status do
 
         options = "-co #{request[:container]}"
@@ -55,7 +55,7 @@ module MCollective
 
 
         apps = apps_raw.split(/^\d/)
-        apps.reject!{&:empty?}
+        apps.reject!{ |x| x.to_s.empty? }
 
         apps.map! do |app|
           app.strip!
@@ -64,6 +64,207 @@ module MCollective
         end
 
         reply[:results]
+      end
+
+      ######## Application Deployment Action
+      action "deploy" do
+
+        # TODO - Check that file exists here
+        # Initialize variables
+        rd, wr = IO.pipe
+
+        # Trying to expose the orphaned proc id the main process
+        child = fork do
+          # Close the read end since it will only write
+          rd.close
+
+          grandchild = fork do
+            # Close write pipe since we don't need it, if not
+            # the read in the main proc will block until this proc
+            # is finished
+            wr.close
+
+            begin
+              # Run our arbitrary shell commands
+              #status = run("/bin/su - ora_ias -c \"/u03/devmidr2/dcm/bin/dcmctl deployApplication -file #{request[:ear_file]}  -a #{request[:a_name]} -co #{request[:c_name]}\"", :stdout => out, :stderr => err)
+              exec("/bin/su - ora_ias -c \"/u03/devmidr2/dcm/bin/dcmctl deployApplication -file #{request[:ear_file]}  -a #{request[:a_name]} -co #{request[:c_name]}\"")
+            rescue SystemCallError
+              1
+            end
+          end
+
+          # Detach the grandchild
+          if grandchild
+            Process.detach(grandchild)
+          end
+
+          # Write grandchild pid to pipe and close to end writing
+          wr.write grandchild
+          wr.close
+        end
+
+        # Detach the child
+        if child
+          Process.detach(child)
+        end
+
+        # Close the writing end
+        wr.close
+
+        # Read grandchild pid value from pipe and close reading end
+        grandchild = rd.read
+        rd.close
+
+        # End forking stuff.  Double forking is officially over.
+
+        if grandchild then
+          reply[:pid] = grandchild
+          reply[:summary] = "Application is being deployed.  Please wait."
+        else
+          reply[:summary] = "Application failed to be deployed.  Please check the nonexistent log files."
+          fail!("Execution failed - deployment not started")
+        end
+
+      end
+
+      # This is Dan's UNDEPLOY action
+      # Input:  c_name (container name)
+      #         a_name (application name)
+      # Output: pid (process identifier)
+      #         summary (status/summary)
+      action "undeploy" do
+
+        # Initialize variables
+        rd, wr = IO.pipe
+
+        # Trying to expose the orphaned proc id the main process
+        child = fork do
+          # Close the read end since it will only write
+          rd.close
+
+          grandchild = fork do
+            # Close write pipe since we don't need it, if not
+            # the read in the main proc will block until this proc
+            # is finished
+            wr.close
+
+            begin
+              # Run our arbitrary shell commands
+              #status = run("/bin/su - ora_ias -c \"/u03/devmidr2/dcm/bin/dcmctl undeployApplication -a #{request[:a_name]} -co #{request[:c_name]}\"", :stdout => what, :stderr => err)
+              exec("/bin/su - ora_ias -c \"/u03/devmidr2/dcm/bin/dcmctl undeployApplication -a #{request[:a_name]} -co #{request[:c_name]}\"")
+            rescue SystemCallError
+              puts "There was an error executing the command. Check the nonexistent log files."
+            end
+          end
+
+          # Detach the grandchild
+          if grandchild
+            Process.detach(grandchild)
+          end
+
+          # Write grandchild pid to pipe and close to end writing
+          wr.write grandchild
+          wr.close
+
+          # This first child still has the TTY of the parent, uncomment to see the
+          # output show up after the main proc is done.
+          #sleep 1
+          #puts "The child of a child (you mean grandchild?) PID is #{grandchild}"
+
+        end
+
+        # Detach the child
+        if child
+          Process.detach(child)
+        end
+
+        # Close the writing end
+        wr.close
+
+        # Read grandchild pid value from pipe and close reading end
+        grandchild = rd.read
+        rd.close
+        # End forking stuff.  Double forking is officially over.
+
+        # Return grandchild's pid
+
+        # Return summary
+        if grandchild then
+          reply[:pid] = grandchild
+          reply[:summary] = "Application is being undeployed.  Please wait."
+        else
+          reply[:summary] = "Application failed to be undeployed.  Please check the nonexistent log files."
+          fail!("Execution failed - deployment not started")
+        end
+
+      end
+
+      # This is Dan's CONTAINER_START action
+      # Input:  c_name (container name)
+      # Output: summary (status/summary)
+      action "container_start" do
+
+        # Initialize variables
+        err = ""
+        out = ""
+
+        # Consider making this a forking function as well since it hovers at the 1.5-2 minute runtime mark
+        status = run("/bin/su - ora_ias -c \"/u03/devmidr2/opmn/bin/opmnctl startproc process-type=#{request[:c_name]}\"", :stdout => out, :stderr => err)
+
+        # Return summary
+        if status == 0 then
+          reply[:summary] = "Container started successfully. Return code #{status}."
+        elsif status == 150 then
+          reply[:summary] = "Container is already running."
+        else
+          reply[:summary] = "Container failed to start with the following message(s): #{out} -- #{err} -- Return code #{status}."
+        end
+
+      end
+
+      ################################## - Container Action
+      action "container_stop" do
+
+        # Initialize variables
+        err = ""
+        out = ""
+
+        status = run("/bin/su - ora_ias -c \"/u03/devmidr2/opmn/bin/opmnctl stopproc process-type=#{request[:c_name]}\"", :stdout => out, :stderr => err)
+
+        if status == 0 then
+          reply[:summary] = "Container stopped successfully. Return code #{status}."
+        elsif status == 150 then
+          reply[:summary] = "Container was not running."
+        else
+          reply[:summary] = "Container failed to start with the following message(s): #{out} -- #{err} -- Return code #{status}."
+        end
+
+      end
+
+      # This is Dan's PIDCHECK action
+      # Input:  pid (process identifier)
+      # Output: status (is it running?)
+      action "pidcheck" do
+
+        # Initialize variables
+        pgid = 0
+
+        begin
+          pgid = Process.getpgid( request[:pid] )
+          true
+        rescue Errno::ESRCH
+          puts "Methinks we have run into an error!  Check the nonexistent log files!"
+          false
+        end
+
+        # Return status
+        if pgid == 0 then
+          reply[:status] = "Process is not running."
+          reply[:status_b] = false
+        else
+          reply[:status] = "Process is still running."
+          reply[:status_b] = true
+        end
       end
     end
   end
